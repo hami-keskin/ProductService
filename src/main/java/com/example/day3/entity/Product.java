0afaec2eb07cd7e99dadb70bfef3eb5ddcb2dd6a
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2024-07-24 14:19:44 +0300

    .

---
+++ src/main/java/com/example/day3/entity/Product.java
@@ -0,0 +1,35 @@
+package com.example.day3.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    // Getters ve Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // No-argument constructor
+    public Product() {
+    }
+}
