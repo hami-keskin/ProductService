07da1fc29fae06c180321805158eca0cbc60becf
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2024-07-24 14:43:40 +0300

    Create ProductService.java

---
+++ src/main/java/com/example/day3/service/ProductService.java
@@ -0,0 +1,45 @@
+package com.example.day3.service;
+
+import com.example.day3.entity.Product;
+import com.example.day3.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product updateProduct(Long id, Product productDetails) {
+        Product product = productRepository.findById(id).orElse(null);
+        if (product != null) {
+            product.setName(productDetails.getName());
+            return productRepository.save(product);
+        } else {
+            return null;
+        }
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    // Tüm ürünleri silen yöntem
+    public void deleteAllProducts() {
+        productRepository.deleteAll();
+    }
+}
