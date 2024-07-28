bc2343be7d8fee64d29003f9e2c84a7ffc563aaf
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2024-07-28 16:53:14 +0300

    .

--- src/main/java/com/example/day3/repository/CatalogRepository.java
+++ src/main/java/com/example/day3/repository/CatalogRepository.java
@@ -3,6 +3,8 @@ package com.example.day3.repository;
 
 import com.example.day3.entity.Catalog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "catalogs", path = "catalogs")
 public interface CatalogRepository extends JpaRepository<Catalog, Integer> {
 }
--- src/main/java/com/example/day3/repository/ProductRepository.java
+++ src/main/java/com/example/day3/repository/ProductRepository.java
@@ -3,9 +3,11 @@ package com.example.day3.repository;
 
 import com.example.day3.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(collectionResourceRel = "products", path = "products")
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     List<Product> findByCatalogId(Integer catalogId);
 }
