39f951d5da73ab4724bfbd447c73bda6f7438770
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2024-08-04 20:21:27 +0300

    +

--- src/main/java/com/example/day3/Day3Application.java
+++ src/main/java/com/example/day3/Day3Application.java
@@ -3,9 +3,11 @@ package com.example.day3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableCaching
+@EnableDiscoveryClient
 public class Day3Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Day3Application.class, args);
