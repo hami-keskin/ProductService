236eb8e9384cd9e1b056ca7be419b354cc767d08
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2024-08-04 21:26:06 +0300

    +

--- src/main/java/com/example/day3/repository/CatalogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.day3.repository;
-
-import com.example.day3.entity.Catalog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CatalogRepository extends JpaRepository<Catalog, Integer> {
-}
