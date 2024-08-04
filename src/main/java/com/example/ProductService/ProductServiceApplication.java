2db4c3590fc4a50b77b9c8aebd93aa6a6c2519a7
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2024-08-05 00:09:05 +0300

    +

--- src/main/java/com/example/ProductService/ProductServiceApplication.java
+++ src/main/java/com/example/ProductService/ProductServiceApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableCaching
 @EnableDiscoveryClient
-public class Day3Application {
+public class ProductServiceApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(Day3Application.class, args);
+		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 }
